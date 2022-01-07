9c4e9c5e3779eab3c38917bc5e2f147e1594fcee
tomoKatanic <tk50151@fer.hr>
2022-01-07 15:59:44 +0100

    fix some synthesis warnings
    *state was multidriven
    *now one always block for states, others are commented
    *fixex inferred latch for all variables except instr

--- riscv_core/riscv_core.srcs/sources_1/new/riscv_core.sv
+++ riscv_core/riscv_core.srcs/sources_1/new/riscv_core.sv
@@ -372,6 +372,8 @@ logic taken_br;
 // For /xreg$value.
 logic [32-1:0] Xreg_value_n1 [31:0],
                Xreg_value_a0 [31:0];
+             
+ 
 
 
 
@@ -415,16 +417,19 @@ generate
   
    // ---------- (2) IMEM -----------------------------------
   // `READONLY_MEM(pc, instr[31:0]);
-    always @(pc) begin
+  
+  /*  always @(pc) begin
         state = FETCH;
         mem_wr_data = 32'b0;
-    end
+    end */
    
    // ---------- (3) DECODE/INSTR_TYPE ----------------------
-   always @(instr) begin
+   
+  /* always @(instr) begin
         state = DECODE;
         mem_wr_data = 32'b0;
-   end
+   end */
+   
    assign is_u_instr = instr[6:2] == 5'b00101 ||
                        instr[6:2] == 5'b01101;
    // or: $is_u_instr = $instr[6:2] ==? 5'b0x101;             
@@ -555,6 +560,8 @@ generate
    //$rf_wr_index[4:0] = $rd;
    //$rf_wr_data[31:0] = $result; 
    // ...
+   
+   /*
    always @ * begin
         if (is_load) begin
             state = LD;
@@ -566,7 +573,7 @@ generate
                 state = FETCH;
             end
         end
-   end
+   end */
 
 
 
@@ -608,12 +615,12 @@ generate
          logic L1_wr;
 
          assign L1_wr = rf_wr_en && (rf_wr_index == xreg);
-         assign L1_wr_is_X = (L1_wr === 1'bX);
          assign Xreg_value_n1[xreg][32-1:0] = reset_a0 ? xreg :
-                                              (L1_wr && !L1_wr_is_X) ? rf_wr_data :
+                                              (L1_wr === 1'b1) ? rf_wr_data :
                                               Xreg_value_a0[xreg][32-1:0];
       end
-      
+ 
+ /*     
   always @ * begin
         if (is_s_instr) begin
             state = STR; 
@@ -626,13 +633,32 @@ generate
             end
       end
    end
+   */
+   
+        always @ * begin
+	       if(is_s_instr) begin
+                state = STR; 
+	       end
+	       else if(is_load) begin
+		        state = LD;
+	       end
+	       else if(reset_a0) begin
+		        state = FETCH;
+	       end
+	       else begin
+		        state = FETCH;	
+	       end
+        end
+   
+   
+   
       //-------------------------------------------------------------------------
       
        
        // for simulation output of all registers
        always_ff @(posedge clk) begin
            if (Xreg_value_a0[5] != 'b0)
-                xreg_out = Xreg_value_a0;          
+                xreg_out <= Xreg_value_a0;          
        end
        
         //----------------------------MEMCONTROL------------------------------------------  
@@ -642,36 +668,40 @@ generate
                     rv_m_addr = pc;
                     rv_m_rw = 1'b0;
                     rv_m_valid = 1'b1;
-                    rv_m_wrdata[32-1:0] = mem_wr_data;
+                    rv_m_wrdata[32-1:0] = 32'b0;
                 end   
                 DECODE: begin
+                    rv_m_addr = pc;
+                    rv_m_rw = 1'b0;
                     rv_m_valid = 1'b0;
-                    //rv_m_rw = 1'bz;
-                    //rv_m_addr = 32'bz;
-                    //rv_m_wdata = 32'bz;
+                    rv_m_wrdata = 32'b0;
                 end
                 LD: begin
+                    rv_m_addr = result;
                     rv_m_rw = 1'b0;
-                    rv_m_addr = mem_rw_addr;
                     rv_m_valid = 1'b1;
-                    rv_m_wrdata[32-1:0] = mem_wr_data;
+                    rv_m_wrdata[32-1:0] = 32'b0;
                 end
-                STR: begin
-                   
+                STR: begin   
+                    rv_m_addr = result;  
                     rv_m_rw = 1'b1;
-                    rv_m_addr = mem_rw_addr; 
                     rv_m_valid = 1'b1;
                     rv_m_wrdata[32-1:0] = src2_value;
                 end
             endcase 
        end
        //----------------------------MEM------------------------------------------
-       always @ (rv_m_rdata) begin
-            if (state == FETCH) begin
+       always @ * begin
+           if (state == FETCH || state == DECODE) begin
                 instr = rv_m_rdata;
-            end 
-            else if(state == LD ) begin
+                ld_data = 32'b0;
+           end 
+           else if(state == LD ) begin
                 ld_data = rv_m_rdata;
+            end
+            else begin
+                //state is STR
+                ld_data = 32'b0;
             end
        end
 
