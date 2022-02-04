9ee12d18718fb035b81e7aebe6e01105f214fb95
tomoKatanic <tk50151@fer.hr>
2022-02-04 14:04:10 +0100

    update to folder conatining all source code files

--- source_code_files/top_soc_tb.sv
+++ source_code_files/top_soc_tb.sv
@@ -26,18 +26,20 @@ module top_soc_tb(
     reg clk;
     reg reset; 
     
-    wire [32-1:0] xreg_out [31:0];   // use these 2 outputs only for Simulation,    
+  //  wire [32-1:0] xreg_out [31:0];   // use these 2 outputs only for Simulation,    
     
-    wire [1 : 0]  m_axi_rresp;
-    wire [1 : 0] m_axi_bresp;
+    wire [1 : 0]  o_axi_rresp;
+    wire [1 : 0] o_axi_rresp1;
     integer i = 1;
+    integer j = 1;
    
    
     top_soc dut(
      .clk(clk), 
      .reset(reset), 
     
-    .xreg_out(xreg_out) // use these 2 outputs only for Simulation,    
+    .o_axi_rresp(o_axi_rresp), // use these 2 outputs only for Simulation, 
+    .o_axi_rresp1(o_axi_rresp1)   
     
     );
     
@@ -58,9 +60,19 @@ module top_soc_tb(
     end
     
     always @ (posedge dut.cpu.rv_m_ready) begin
-            $display("num_clk is %d", i);
+            $display("num_clk is i:%d", i);
             i <= 1;
     end
+    
+     always @ (posedge clk or posedge dut.cpu1.rv_m_ready) begin
+            j <= j + 1;
+    end
+    
+    always @ (posedge dut.cpu1.rv_m_ready) begin
+            $display("num_clk is J: %d", j);
+            j <= 1;
+    end
+    
 
 
 
